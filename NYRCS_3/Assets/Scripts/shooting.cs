0ce5ec6273a8ce978916e2dce7bbc3ecc8e8188f
SethLam2007 <sethlam2007@gmail.com>
2024-06-10 14:43:02 +0800

    shooting scripts

--- NYRCS_3/Assets/Scripts/shooting.cs
+++ NYRCS_3/Assets/Scripts/shooting.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform gunBarrel;
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 30f;
+    public float fireRate = 0.5f;
+    private float nextFireTime;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1")) & nextFireTime.time >= nextFireTime
+
+        {
+
+            Shoot();
+            nextFireTime = nextFireTime.time + 1f / fireRate;
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, gunBarrel.position, gunBarrel.rotation);
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+        if (buletRB !=null)
+        {
+            bulletRb.velocity = gunBarrel.forward * bulletSpeed;
+        }
+        else
+        {
+            Debug.LogError("Bullet prefab does not have rigidbody")
+        }
     }
 }
